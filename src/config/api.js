60d887198a1b6a75c1d2629d68143afbcceb5b78
pillar008 <sharmaneeraj4759@gmail.com>
2025-01-13 12:35:47 +0530

    initial commit

---
+++ src/config/api.js
@@ -0,0 +1,7 @@
+const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
+const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
+const API_KEY = "31c565d2b7f03eed870c2fbb8731241f";
+export const endpointPath = (country, category) =>
+  `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
+export const endpointSearch = (searchQuery) =>
+  `${API_SEARCH_DOMAIN}${searchQuery}&lang=en&apikey=${API_KEY}`;
